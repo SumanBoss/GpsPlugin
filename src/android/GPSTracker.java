853ad30b6e162ebf09c7557fc8d84745d13610e7
Suman Jana <suman.jana2@gmail.com>
2013-12-27 17:13:21 +0530

    Update GPSTracker.java

--- src/android/GPSTracker.java
+++ src/android/GPSTracker.java
@@ -3,114 +3,41 @@ package org.apache.cordova.gpsplugin;
 import org.json.JSONArray;
 import org.json.JSONException;
 import android.content.Context;
-import android.net.wifi.ScanResult;
-import android.net.wifi.WifiConfiguration;
-import android.net.wifi.WifiInfo;
-import android.net.wifi.WifiManager;
-import android.telephony.TelephonyManager;
-import java.util.List;
 import org.json.JSONObject;
 import android.widget.Toast;
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
 
-public class WifiInfoPlugin extends CordovaPlugin {
+public class GPSTracker extends CordovaPlugin {
 
     public static final String ACTION_MANAGE_WIFI = "managewifi";
     public static final String GET_DEVICE = "getdevice";
-    public static final String WIFI_INFO = "wifinfo";
-    public static final String CHANGE_WIFI = "changewifi";
-    public static String imei;
-    public static String macadress;
-    public static String provider;
-    public static String phonenumber;
-    public static int wifistatus;
+    public static final String GPS_INFO = "gpsinfo";
+    public static final String CHANGE_GPS = "changegps";
+    public static int gpsstatus;
 
-    public WifiInfoPlugin() {
+    public GPSTracker() {
     }
 
     @Override
     public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
         Context context = cordova.getActivity().getApplicationContext();
         try {
-            if (ACTION_MANAGE_WIFI.equals(action)) {
-                WifiManager wifi = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
-                if (wifi.isWifiEnabled() == false) {
-                    wifi.setWifiEnabled(true);
-                }
-
-                List<ScanResult> mScanResults = wifi.getScanResults();
-                for (ScanResult result : mScanResults) {
- Toast.makeText(context, "sdsds====" + data.length(), Toast.LENGTH_LONG).show();
-                    for (int i = 0; i < data.length(); i++) {
-                        JSONObject oneObject = data.getJSONObject(i);
-                        String ssid = oneObject.getString("ssid");
-                        Toast.makeText(context, "ssid====" + ssid, Toast.LENGTH_LONG).show();
-                        String password = oneObject.getString("password");
-                        if (ssid.equals(result.SSID)) {
-                            WifiConfiguration wc = new WifiConfiguration();
-//                            wc.SSID = "\"belkin_cloudlabz\"";
-//                            wc.preSharedKey = "\"84dcd64a\"";
-                            wc.SSID = ssid;
-                            wc.preSharedKey = password;
-                            wc.hiddenSSID = true;
-                            wc.status = WifiConfiguration.Status.ENABLED;
-
-                            wc.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.TKIP);
-                            wc.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.CCMP);
-                            wc.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.WPA_PSK);
-                            wc.allowedPairwiseCiphers.set(WifiConfiguration.PairwiseCipher.TKIP);
-                            wc.allowedPairwiseCiphers.set(WifiConfiguration.PairwiseCipher.CCMP);
-                            wc.allowedProtocols.set(WifiConfiguration.Protocol.RSN);
-                            int res = wifi.addNetwork(wc);
-                            boolean es = wifi.saveConfiguration();
-                            boolean b = wifi.enableNetwork(res, true);
-                            break;
-                        }
-                    }
-                }
-                callbackContext.success();
-                return true;
-            } else if (GET_DEVICE.equals(action)) {
-                TelephonyManager telephonyManager = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
-                imei = telephonyManager.getDeviceId();
-                phonenumber = telephonyManager.getLine1Number();
-                provider = telephonyManager.getNetworkOperatorName();
-                WifiManager wifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
-                if (wifiManager.isWifiEnabled()) {
-                    WifiInfo info = wifiManager.getConnectionInfo();
-                    macadress = info.getMacAddress();
+            if (GPS_INFO.equals(action)) {
+               GPSManager gps = new GPSManager(context);
+                if (gps.canGetLocation() == false) {
+                    gpsstatus=0;
                 } else {
-                    wifiManager.setWifiEnabled(true);
-                    WifiInfo info = wifiManager.getConnectionInfo();
-                    macadress = info.getMacAddress();
-                    wifiManager.setWifiEnabled(false);
+                   gpsstatus=1;
                 }
                 JSONObject jSONObject = new JSONObject();
-                jSONObject.put("imei", WifiInfoPlugin.imei);
-                jSONObject.put("macadress", WifiInfoPlugin.macadress);
-                jSONObject.put("phonenumber", WifiInfoPlugin.phonenumber);
-                jSONObject.put("provider", WifiInfoPlugin.provider);
+                 Toast.makeText(context.getApplicationContext(),gpsstatus , Toast.LENGTH_LONG).show();
+                jSONObject.put("status", GPSTracker.gpsstatus);
                 callbackContext.success(jSONObject);
                 return true;
-            } else if (WIFI_INFO.equals(action)) {
-                WifiManager wifi = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
-                if (wifi.isWifiEnabled() == false) {
-                    wifistatus=0;
-                } else {
-                   wifistatus=1;
-                }
-                JSONObject jSONObject = new JSONObject();
-                jSONObject.put("status", WifiInfoPlugin.wifistatus);
-                callbackContext.success(jSONObject);
-                return true;
-            } else if (CHANGE_WIFI.equals(action)) {
-                WifiManager wifi = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
-                if (wifi.isWifiEnabled() == false) {
-                    wifi.setWifiEnabled(true);
-                } else {
-                    wifi.setWifiEnabled(false);
-                }
+            } else if (CHANGE_GPS.equals(action)) {
+                GPSManager gps = new GPSManager(context);
+                gps.showSettingsAlert();
                 callbackContext.success();
                 return true;
             } else {
