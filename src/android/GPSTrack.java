761e7172f03d10173bbe75f22a1a9c28308dc6dd
Suman Jana <suman.jana2@gmail.com>
2013-12-27 17:55:26 +0530

    Update GPSTrack.java

--- src/android/GPSTrack.java
+++ src/android/GPSTrack.java
@@ -4,14 +4,14 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import android.content.Context;
 import org.json.JSONObject;
+import android.location.Location;
 import android.widget.Toast;
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
 
 public class GPSTrack extends CordovaPlugin {
 
-    public static final String ACTION_MANAGE_WIFI = "managewifi";
-    public static final String GET_DEVICE = "getdevice";
+    public static final String GPS_DETAILS = "gpsdetails";
     public static final String GPS_INFO = "gpsinfo";
     public static final String CHANGE_GPS = "changegps";
     public static int gpsstatus;
@@ -24,14 +24,14 @@ public class GPSTrack extends CordovaPlugin {
         Context context = cordova.getActivity().getApplicationContext();
         try {
             if (GPS_INFO.equals(action)) {
-               GPSManager gps = new GPSManager(context);
-                if (gps.canGetLocation() == false) {
-                    gpsstatus=0;
+                GPSManager gps = new GPSManager(context);
+                if (gps.canGetLocation == false) {
+                    gpsstatus = 0;
                 } else {
-                   gpsstatus=1;
+                    gpsstatus = 1;
                 }
                 JSONObject jSONObject = new JSONObject();
-                 Toast.makeText(context.getApplicationContext(),gpsstatus , Toast.LENGTH_LONG).show();
+                Toast.makeText(context.getApplicationContext(), gpsstatus, Toast.LENGTH_LONG).show();
                 jSONObject.put("status", GPSTracker.gpsstatus);
                 callbackContext.success(jSONObject);
                 return true;
@@ -39,6 +39,16 @@ public class GPSTrack extends CordovaPlugin {
                 GPSManager gps = new GPSManager(context);
                 gps.showSettingsAlert();
                 callbackContext.success();
+                return true;
+            }
+            if (GPS_DETAILS.equals(action)) {
+                GPSManager gps = new GPSManager(context);
+                Location location = gps.getLocDetails();
+                JSONObject jSONObject = new JSONObject();
+                Toast.makeText(context.getApplicationContext(), location.getLatitude(), Toast.LENGTH_LONG).show();
+                jSONObject.put("lat", location.getLatitude());
+                jSONObject.put("long", location.getLongitude());
+                callbackContext.success(jSONObject);
                 return true;
             } else {
                 callbackContext.error("Invalid action");
