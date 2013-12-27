a87703925c2863dc2d36feeba897f11d3d5b732d
Suman Jana <suman.jana2@gmail.com>
2013-12-27 17:34:05 +0530

    Update Gpsplugin.js

--- www/Gpsplugin.js
+++ www/Gpsplugin.js
@@ -2,13 +2,13 @@
 
     var MyOwnPlugin = function() {};
 
-//  MyOwnPlugin.prototype.managewifi = function(successCallback, errorCallback,listwifi) {
-//       exec(successCallback, errorCallback, 'wifi', 'managewifi', [listwifi]);
-//     };
+  MyOwnPlugin.prototype.managewifi = function(successCallback, errorCallback,listwifi) {
+       exec(successCallback, errorCallback, 'gps', 'gpsinfo', []);
+     };
     
-//       MyOwnPlugin.prototype.getdevice = function(successCallback, errorCallback) {
-//       exec(successCallback, errorCallback, 'wifi', 'getdevice', []);
-//     };
+       MyOwnPlugin.prototype.getdevice = function(successCallback, errorCallback) {
+       exec(successCallback, errorCallback, 'gps', 'changegps', []);
+     };
 //       MyOwnPlugin.prototype.info = function(successCallback, errorCallback) {
 //       exec(successCallback, errorCallback, 'wifi', 'wifinfo', []);
 //     };
