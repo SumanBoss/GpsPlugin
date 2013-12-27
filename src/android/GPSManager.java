16dfe0e1be925aa858e7cda9dd4402486c260a17
Suman Jana <suman.jana2@gmail.com>
2013-12-27 17:51:13 +0530

    Update GPSManager.java

--- src/android/GPSManager.java
+++ src/android/GPSManager.java
@@ -11,8 +11,6 @@ import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.provider.Settings;
-import android.util.Log;
-
 /**
  *
  * @author Suman Jana
@@ -47,8 +45,7 @@ public class GPSManager extends Service implements LocationListener {
         this.mContext = context;
         getLocation();
     }
-
-    public Location getLocation() {
+public Location getLocDetails() {
         try {
             locationManager = (LocationManager) mContext
                     .getSystemService(LOCATION_SERVICE);
@@ -65,30 +62,12 @@ public class GPSManager extends Service implements LocationListener {
                 // no network provider is enabled
             } else {
                 this.canGetLocation = true;
-                // First get location from Network Provider
-//                if (isNetworkEnabled) {
-//                    locationManager.requestLocationUpdates(
-//                            LocationManager.NETWORK_PROVIDER,
-//                            MIN_TIME_BW_UPDATES,
-//                            MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
-//                    Log.d("Network", "Network");
-//                    if (locationManager != null) {
-//                        location = locationManager
-//                                .getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
-//                        if (location != null) {
-//                            latitude = location.getLatitude();
-//                            longitude = location.getLongitude();
-//                        }
-//                    }
-//                }
-                // if GPS Enabled get lat/long using GPS Services
                 if (isGPSEnabled) {
                     if (location == null) {
                         locationManager.requestLocationUpdates(
                                 LocationManager.GPS_PROVIDER,
                                 MIN_TIME_BW_UPDATES,
                                 MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
-                        Log.d("GPS Enabled", "GPS Enabled");
                         if (locationManager != null) {
                             location = locationManager
                                     .getLastKnownLocation(LocationManager.GPS_PROVIDER);
@@ -105,6 +84,28 @@ public class GPSManager extends Service implements LocationListener {
         }
 
         return location;
+    }
+       public void getLocation() {
+        try {
+            locationManager = (LocationManager) mContext
+                    .getSystemService(LOCATION_SERVICE);
+
+            // getting GPS status
+            isGPSEnabled = locationManager
+                    .isProviderEnabled(LocationManager.GPS_PROVIDER);
+
+            // getting network status
+            isNetworkEnabled = locationManager
+                    .isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+
+            if (!isGPSEnabled) {
+                // no network provider is enabled
+            } else {
+                this.canGetLocation = true;
+            }
+
+        } catch (Exception e) {
+        }
     }
 
     /**
