fcb6f20e271a7cb92773aae6fee760d0a0ff3c33
Suman Jana <suman.jana2@gmail.com>
2013-12-27 16:36:07 +0530

    Create Gpsplugin.js

---
+++ www/Gpsplugin.js
@@ -0,0 +1,18 @@
+    var exec = require('cordova/exec');
+
+    var MyOwnPlugin = function() {};
+
+ MyOwnPlugin.prototype.managewifi = function(successCallback, errorCallback,listwifi) {
+       exec(successCallback, errorCallback, 'wifi', 'managewifi', [listwifi]);
+    };
+    
+      MyOwnPlugin.prototype.getdevice = function(successCallback, errorCallback) {
+       exec(successCallback, errorCallback, 'wifi', 'getdevice', []);
+    };
+      MyOwnPlugin.prototype.info = function(successCallback, errorCallback) {
+       exec(successCallback, errorCallback, 'wifi', 'wifinfo', []);
+    };
+     MyOwnPlugin.prototype.changewifi = function(successCallback, errorCallback) {
+       exec(successCallback, errorCallback, 'wifi', 'changewifi', []);
+    };
+    module.exports = new MyOwnPlugin();
