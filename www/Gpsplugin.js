f46846493929e23280f08d144e9765ba4ad87508
Suman Jana <suman.jana2@gmail.com>
2013-12-27 17:22:04 +0530

    Update Gpsplugin.js

--- www/Gpsplugin.js
+++ www/Gpsplugin.js
@@ -2,17 +2,17 @@
 
     var MyOwnPlugin = function() {};
 
- MyOwnPlugin.prototype.managewifi = function(successCallback, errorCallback,listwifi) {
-       exec(successCallback, errorCallback, 'wifi', 'managewifi', [listwifi]);
-    };
+//  MyOwnPlugin.prototype.managewifi = function(successCallback, errorCallback,listwifi) {
+//       exec(successCallback, errorCallback, 'wifi', 'managewifi', [listwifi]);
+//     };
     
-      MyOwnPlugin.prototype.getdevice = function(successCallback, errorCallback) {
-       exec(successCallback, errorCallback, 'wifi', 'getdevice', []);
-    };
-      MyOwnPlugin.prototype.info = function(successCallback, errorCallback) {
-       exec(successCallback, errorCallback, 'wifi', 'wifinfo', []);
-    };
-     MyOwnPlugin.prototype.changewifi = function(successCallback, errorCallback) {
-       exec(successCallback, errorCallback, 'wifi', 'changewifi', []);
-    };
+//       MyOwnPlugin.prototype.getdevice = function(successCallback, errorCallback) {
+//       exec(successCallback, errorCallback, 'wifi', 'getdevice', []);
+//     };
+//       MyOwnPlugin.prototype.info = function(successCallback, errorCallback) {
+//       exec(successCallback, errorCallback, 'wifi', 'wifinfo', []);
+//     };
+//      MyOwnPlugin.prototype.changewifi = function(successCallback, errorCallback) {
+//       exec(successCallback, errorCallback, 'wifi', 'changewifi', []);
+//     };
     module.exports = new MyOwnPlugin();
